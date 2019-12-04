a55c45abaeab0185fbccccf23e74eee20ea9a781
mmarcielle <54721441+mmarcielle@users.noreply.github.com>
2019-12-04 12:03:00 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# CPPTemplate
+A jumping off point for creating a small C++ console application
+
+## Instructions
+1. Copy
+    1. Create a new repository which uses this one as a template
+    2. Fork this repository
+2. Initial commit
+    1. Change the filename from `template.cpp`
+    2. Fill in the header `TODO`
+    3. Commit
+3. Subsequent commits
+    1. Fill out `TODO` sections using the design recipe
+    2. Commit whenever you have completed a design recipe, added new tests, implemented a function, etc.
---
+++ template.cpp
@@ -0,0 +1,71 @@
+/**
+ * 
+ * TODO: Standard Header
+ * Name:
+ * Date:
+ * Program summary
+ **/
+
+/**
+ * TODO: Includes
+ * Include any additional libraries
+ **/
+#include <iostream>
+#include <cassert>
+#include <string>
+
+using namespace std;
+
+/**
+ * Data Definitions 
+ * TODO: Write definitions for any data here
+ * EG:
+ * enum class Space {Empty, X, O};
+ * struct Row {
+ *   Space Left, Center, Right;
+ * }
+ **/
+
+/**
+ * Function Declarations
+ * TODO: Inert your function declarations, purpose statements, I/O comments here.
+ **/
+
+/**
+ * Declarations for given functions. Don't touch.
+ **/
+void test();
+void run();
+
+
+/**
+ * Main Function
+ * Don't touch this! use `run()` instead
+ **/
+int main() {
+  char t;
+  cout << "Enter [t] to test, or any other character to run." << endl;
+  cin >> t;
+  if(t == 't') {
+    test();
+  } else {
+    run();
+  }
+}
+
+/**
+ * Function Definitions
+ *  Call a test function for each of your functions
+ **/
+void test() {
+  // TODO: call your test functions here.
+}
+
+// Use this in place of a main function
+void run() {
+  // TODO: write your point of entry code here.
+}
+
+/**
+ * TODO: Insert your function definitions and unit test function definitions here.
+ **/
